0b82a3134ba6d5052ac538be46f9c0df1f4849ed
shalk <xshalk@163.com>
2013-11-07 13:45:51 +0800

    Update create_cvm_and_coc.sh

--- master/create_cvm_and_coc.sh
+++ master/create_cvm_and_coc.sh
@@ -10,7 +10,7 @@ virt-install    --hvm   --name cvm  --ram 4096  --vcpus 4 \
  --disk  path=${cvmcocpath}/cvm.raw   --os-type "linux"  --network bridge=br0 --boot hd
  
 # create coc
-cp -rf  ${cvmcocpath}/cvm.raw  ${cvmcocpath}/coc.raw 
+qemu-img convert -f qcow2  ../../cvm.qcow2  -O raw  ${cvmcocpath}/coc.raw 
 
 virt-install    --hvm   --name coc --ram 4096  --vcpus 4 \
  --disk  path=${cvmcocpath}/coc.raw   --os-type "linux"  --network bridge=br0 --boot hd
